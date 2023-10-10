85b2ddab0febe20d27cb02fbd009d4bb420ef426
Maxim Sedykh <max_se@bk.ru>
2023-10-10 14:41:59 +0300

    Сделал сортировку отзывов по двум критериям

---
+++ SQLServerCourse/Controllers/FinalExamController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SQLServerCourse.Controllers
+{
+    public class FinalExamController : Controller
+    {
+        [HttpGet]
+        public IActionResult PassExam()
+        {
+            return View();
+        }
+
+        public IActionResult GetLearningResult()
+        {
+            return View();
+        }
+
+        public IActionResult GetAnalys()
+        {
+            return PartialView();
+        }
+
+        public IActionResult GetExamResult()
+        {
+            return PartialView();
+        }
+    }
+}
