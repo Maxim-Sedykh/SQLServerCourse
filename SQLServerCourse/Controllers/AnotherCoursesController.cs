a87ddf8c2a7b373b9c6af582085855fe1a7a1e78
Maxim Sedykh <max_se@bk.ru>
2023-10-11 00:43:45 +0300

    Изменил дизайн Layout и Index

--- SQLServerCourse/Controllers/AnotherCoursesController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace SQLServerCourse.Controllers
-{
-    public class AnotherCoursesController : Controller
-    {
-        public IActionResult GetAnotherCourses() => View();
-    }
-}
