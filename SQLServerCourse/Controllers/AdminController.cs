76e3c51123d9a896a4d6f2bdcaf136f00d16fa97
Maxim Sedykh <max_se@bk.ru>
2023-11-02 00:26:26 +0300

    Провёл инициализацию контроллера "Админ"

---
+++ SQLServerCourse/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLServerCourse.DAL;
+using SQLServerCourse.Service.Implementations;
+using SQLServerCourse.Service.Interfaces;
+
+namespace SQLServerCourse.Controllers
+{
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+            
+        }
+
+        public IActionResult ChangeUserData()
+        {
+            return View();
+        }
+
+
+        public IActionResult ChangeLessonData()
+        {
+            return View();
+        }
+    }
+}
